e704854bb67500549a64c41225198991ee6613f1
tutaershen <1835362387@qq.com>
2022-09-09 14:30:29 +0800

    初始化一个springboot项目

---
+++ src/test/java/com/example/githouse/GithouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
